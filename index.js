659d2e2cd41ef17c8174357a1a95ba015ef69ee6
Mercena2007 <102794864+Mercena2007@users.noreply.github.com>
2022-08-27 17:45:09 +0300

    index.js update

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js");
 const colors = require("colors");
 const config = require("./config.json")
 const OS = require('os')
+const enmap = require("enmap");
+const fs = require("fs");
+const Events = require("events");
+const { delay } = require("./handlers/functions")
 require('dotenv').config()
 
 const client = new Discord.Client({
