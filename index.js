186c50bc067407e35f50f1cbb411b8f280776106
Mercena2007 <102794864+Mercena2007@users.noreply.github.com>
2022-08-27 17:04:30 +0300

    index.js update

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const colors = require("colors");
+const config = require("./config.json")
+const OS = require('os')
+require('dotenv').config()
+
+const client = new Discord.Client({
+    fetchAllMembers: false,
+    restTimeOffset: 0,
+    failIfNotExists: false,
+    shards: "auto",
+    allowedMentions: {
+        parse: ["roles", "users"],
+        repliedUser: false,
+    },
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
+    intents: [Discord.Intents.FLAGS.GUILDS,
+              Discord.Intents.FLAGS.GUILD_MEMBERS,
+              Discord.Intents.FLAGS.GUILD_BANS,
+              Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+              Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+              Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+              Discord.Intents.FLAGS.GUILD_INVITES,
+              Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+              Discord.Intents.FLAGS.GUILD_PRESENCES,
+              Discord.Intents.FLAGS.GUILD_MESSAGES,
+              Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+              Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+              Discord.Intents.FLAGS.DIRECT_MESSAGES,
+              Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+              Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+  ],
+  presence: {
+    activities: [{name: `${config.status.text}`.replace("{prefix}", config.prefix), type: config.status.type, url: config.status.url}],
+    status: "online"
+  }
+});
+
+//BOT WAS CODED BY Mercena2007//
